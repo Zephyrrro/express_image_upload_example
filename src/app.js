894ba1a067ee7494cbeff563e128037a54a5f406
cucu6 <1103452257@qq.com>
2019-12-07 12:25:46 +0800

    first commit

---
+++ src/app.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const multer = require("multer");
+const fs = require("fs");
+const path = require("path");
+const port = process.env.PORT || 7777;
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, path.join(__dirname, "static"));
+  },
+  filename: (req, file, callback) => {
+    const filename = file.originalname;
+    const extName = filename.slice(filename.lastIndexOf("."));
+    callback(null, `${file.fieldname}-${Date.now()}${extName}`);
+  }
+});
+
+const upload = multer({ storage });
+
+app.get("/", (req, res) => {
+  res
+    .status(200)
+    .sendFile("index.html", { root: path.join(__dirname, "/public") });
+    res.send
+});
+
+app.get("/image/", (req, res, next) => {
+  const filename = req.query.filename;
+  try {
+    //  判断文件是否存在
+    fs.statSync(path.join(__dirname, `static`, filename));
+    console.log("file exist");
+    res.sendFile(path.join(__dirname, `static`, filename));
+  } catch {
+    console.log("file doesn't exist");
+    next();
+  }
+});
+
+app.post("/uploadImage", upload.single("upload"), (req, res) => {
+  const file = req.file;
+  res.send(file);
+});
+
+app.get("*", (req, res) => {
+  console.log("404 handler");
+  res.status(404).sendFile(path.join(__dirname, "public", `404.html`));
+});
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
