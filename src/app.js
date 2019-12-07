9b4defb275ea242c66087b4b41e5f0bb442c8f1f
cucu6 <1103452257@qq.com>
2019-12-07 15:46:14 +0800

    feat: CORS fix

--- src/app.js
+++ src/app.js
@@ -24,11 +24,22 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 
+app.all("*", function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type,Content-Length, Authorization, Accept,X-Requested-With"
+  );
+  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+  res.header("X-Powered-By", " 3.2.1");
+  next();
+});
+
 app.get("/", (req, res) => {
   res
     .status(200)
     .sendFile("index.html", { root: path.join(__dirname, "/public") });
-    res.send
+  res.send;
 });
 
 app.get("/image/", (req, res, next) => {
